6892c0689abc1755a6ce163c7198fac5e8dbdc49
NitinBhasneia <nitinbhasneria360@gmail.com>
2019-05-20 14:56:23 +0000

    Added header and column

--- page/src/App.js
+++ page/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from "./components/header";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Col from "./components/col";
+import Footer from "./components/footer"
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <div className="Side1">
+         <Col/>
+         </div>
+         <div className="Side2">
+         <Header/>
+         <Footer/>
+         </div>
+      </div>
+    );
+  }
 }
-
 export default App;
