0a4fd6dc6c4588ea833935325dd3939612dfc1a6
NitinBhasneia <nitinbhasneria360@gmail.com>
2019-05-21 12:15:30 +0000

    Added Footer

--- page/src/components/footer.js
+++ page/src/components/footer.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
-
+import './col.css';
 class Footer extends Component{
+    state={
+	    buttons:[ 
+	         {bname:'HOME'},
+	         {bname:'COMPANY'},
+	         {bname:'PORTFOLIO'},
+	         {bname:'BLOG'}
+	             ]
+    };
+    newButtoon(){
+	     return this.state.buttons.map((bname,index)=>
+	     	
+		         <p className='b4' key={index}>{this.state.buttons[index].bname}</p>
+		         )}
 	render() {
-		return (
-			<div></div>
-		);
+		 return (<div className='footer'> 
+			    {this.newButtoon()}
+			    <div className='last'>
+			    <p className='l'>2019</p><p className='light'>Creative Tim</p><p>, made with love for a better web</p>
+			   </div>
+			   </div>
+		)
 	}
 }
 
