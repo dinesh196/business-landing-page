7ec934b949cbf7d52e9f5220dae270c68d18fd96
Kunal Mohan <kunalmohan99@gmail.com>
2019-05-26 21:30:36 +0530

    employee stats complete

--- page/src/components/empstats.js
+++ page/src/components/empstats.js
@@ -3,6 +3,33 @@ import './empstats.css';
 
 class EmpStats extends Component{
 	render(){
+		let emps = [
+					{
+						"id":1,
+						"name":"Dakota Rice",
+						"salary":"$36,738",
+						"country":"Niger"
+					},
+					{
+						"id":2,
+						"name":"Minerva Hooper",
+						"salary":"$23,789",
+						"country":"Curaçao"
+					},
+					{
+						"id":3,
+						"name":"Sage Rodriguez",
+						"salary":"$56,142",
+						"country":"Netherlands"
+					},
+					{
+						"id":4,
+						"name":"Philip Chaney",
+						"salary":"$38,735",
+						"country":"Korea, South"
+					}
+					];
+
 		return(
 			<div className="stats">
 				<div className="stats-head">
@@ -10,9 +37,40 @@ class EmpStats extends Component{
 					<div className="stats-head-2">New employees on 15th September, 2016</div>
 				</div>
 				<div className="stats-content">
+				<Content emps={emps} />
 				</div>
 			</div>
 			);
+	}
+}
+
+function ReturnEmp(props){
+	return(
+		<tr>
+			<td>{props.emp.id}</td>
+			<td>{props.emp.name}</td>
+			<td>{props.emp.salary}</td>
+			<td>{props.emp.country}</td>
+		</tr>
+	);
+}
+
+class Content extends Component{
+	render(){
+		let contentEmps = this.props.emps.map(abc => <ReturnEmp emp={abc} key={abc.id}/>);
+		return(
+			<table cellspacing="0px">
+				<tbody>
+					<tr>
+						<td className="row-head"> ID</td>
+						<td className="row-head">Name</td>
+						<td className="row-head">Salary</td>
+						<td className="row-head">Country</td>
+					</tr>
+					{contentEmps}
+				</tbody>
+			</table>
+		);
 	}
 }
 
--- page/src/components/tasks.js
+++ page/src/components/tasks.js
@@ -37,15 +37,6 @@ function ReturnTask(props){
 }
 
 class Content extends Component{
-	constructor(props){
-		super(props);
-	/*	this.state = {
-			items: ['Sign contract for "What are conference organizers afraid of?"',
-					'Lines From Great Russian Literature? Or E-mails From My Boss?',
-					'Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit',
-					'Create 4 Invisible User Experiences you Never Knew About']
-		}*/
-	}
 
 	render(){
 		let contentItems = this.props.items.map(abc => <ReturnTask item={abc} key={abc}/>);
